d0ab3512e3ab799b10eb454c491ac976066e31b0
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-26 13:32:14 -0800

    Added fields to Category.cs.

---
+++ OnlineShop/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
