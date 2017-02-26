4aa94091884d54209850b4867cf7696635f6a953
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-26 14:59:24 -0800

    Built MockPieRepository Entity.

--- OnlineShop/Models/MockPieRepository.cs
+++ OnlineShop/Models/MockPieRepository.cs
@@ -6,8 +6,30 @@ using System.Threading.Tasks;
 
 namespace OnlineShop.Models
 {
-    public class MockPieRepository
+    public class MockPieRepository: IPieRepository
     {
-        
+        private readonly ICategoryRepository _categoryRepository = new MockCategoryRepository();
+
+        public IEnumerable<Pie> Pies
+        {
+            get
+            {
+                return new List<Pie>
+                {
+                    new Pie {PieId = 1, Name="Strawberry Pie", Price=15.95M, ShortDescription },
+                    new Pie {PieId = 1, Name="Strawberry Pie", Price=15.95M, ShortDescription },
+                    new Pie {PieId = 1, Name="Strawberry Pie", Price=15.95M, ShortDescription },
+                    new Pie {PieId = 1, Name="Strawberry Pie", Price=15.95M, ShortDescription },
+
+                };
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        public Pie GetPieById(int pieId)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
