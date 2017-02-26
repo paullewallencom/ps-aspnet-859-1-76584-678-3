9a42d2c06d5f1281db92b6aad2814acc78bcd1ee
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-26 15:36:43 -0800

    Binding with ViewModel.

--- OnlineShop/Controllers/PieController.cs
+++ OnlineShop/Controllers/PieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Models;
+using OnlineShop.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,7 +23,10 @@ namespace OnlineShop.Controllers
 
         public ViewResult List()
         {
-            ViewBag.CurrentCategory = "Cheese cakes";
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.Pies;
+
+            piesListViewModel.CurrentCategory = "Cheese cakes";
             return View(_pieRepository.Pies);
         }
     }
