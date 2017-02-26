cc277f14a0cb1fa994236cf02703ec5246830456
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-26 15:07:48 -0800

    Registered services.

--- OnlineShop/Startup.cs
+++ OnlineShop/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using OnlineShop.Models;
 
 namespace OnlineShop
 {
@@ -16,6 +17,8 @@ namespace OnlineShop
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, MockPieRepository>();
             services.AddMvc();
         }
 
