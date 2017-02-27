77ad1475deadea5854c21f0d72ecdc943bb8086e
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-27 09:24:16 -0800

    Built ShoppingCartItem.cs.

---
+++ OnlineShop/Models/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Models
+{
+    public class ShoppingCartItem
+    {
+        public int ShoppingCartItemId { get; set; }
+        public Pie Pie { get; set; }
+        public int Amount { get; set; }
+        public string ShoppingCartId { get; set; }
+    }
+}
