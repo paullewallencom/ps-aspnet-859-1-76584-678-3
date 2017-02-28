da977cf7968f3c70b2833baff9efba948fcc79cf
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-28 13:44:38 -0800

    Built HomeController.cs.

--- OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,23 @@ using System.Threading.Tasks;
 
 namespace OnlineShop.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
     }
 }
