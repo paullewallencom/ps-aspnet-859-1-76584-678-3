1ec98a16e184ca2cc0bc1b0dc52264f6384b1fb0
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-28 11:34:58 -0800

    Building ShoppingCartController.cs.

--- OnlineShop/Models/ShoppingCartController.cs
+++ OnlineShop/Models/ShoppingCartController.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OnlineShop.Models;
 
 namespace OnlineShop.Models
 {
-    public class ShoppingCartController
+    public class ShoppingCartController : Controller
     {
-        
+        private IPieRepository _pieRepository;
+        private ShoppingCart _shoppingCart;
+
+        public ShoppingCartController(IPieRepository pieRepository, ShoppingCart shoppingCart)
+        {
+            _pieRepository = pieRepository;
+            _shoppingCart = shoppingCart;
+        }
     }
 }
