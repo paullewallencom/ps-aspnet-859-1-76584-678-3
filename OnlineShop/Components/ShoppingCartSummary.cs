0e504491bf3236aed907ba7255bc111698bf57f0
Paul Lewallen <paul.lewallen@gmail.com>
2017-02-28 14:28:04 -0800

    Built ShoppingCartSummary Component.

--- OnlineShop/Components/ShoppingCartSummary.cs
+++ OnlineShop/Components/ShoppingCartSummary.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,26 @@ using System.Threading.Tasks;
 
 namespace OnlineShop.Components
 {
-    public class ShoppingCartSummary
+    public class ShoppingCartSummary: ViewComponent
     {
-        
+        private readonly ShoppingCartSummary _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCartSummary shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCartSummary = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+        }
     }
 }
